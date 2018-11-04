65e58922aa826c7ffae8d7510cfa4451602aaa15
szkertesz <sz.kertesz@mito.hu>
2018-11-04 19:54:15 +0100

    Add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
                 path: `${__dirname}/src/`,
             },
         },
-        `gatsby-transformer-remark`
+        `gatsby-transformer-remark`,
+        `gatsby-plugin-netlify-cms`
     ]
 }
