7a4cb76929061b193e949e280f035178fb4a14ae
Szabolcs Kertész <sz.kertesz@mito.hu>
2018-11-14 16:05:13 +0100

    Add SCSS  & postCSS compiler

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,22 @@ module.exports = {
         path: `${__dirname}/blog`
       }
     },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+          // includePaths: ["absolute/path/a", "absolute/path/b"],
+          precision: 8
+      },
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+            require(`autoprefixer`)({}),
+            require(`cssNano`)({})
+        ]
+      },
+    },
     `gatsby-transformer-remark`,
     `gatsby-plugin-netlify-cms`
   ]
